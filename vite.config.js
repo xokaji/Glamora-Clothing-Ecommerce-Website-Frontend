52b4868c293d31119738df22f57e7153e7a344df
xokaji <rmahiti2@gmail.com>
2025-01-26 20:43:38 +0530

    New

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { reactRouter } from "@react-router/dev/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    reactRouter(),
+    tsconfigPaths(),
+  ],
+});
