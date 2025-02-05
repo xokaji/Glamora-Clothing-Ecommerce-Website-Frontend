3aefe597820f84f4c8a757e732c9ec31bb8c888d
xokaji <rmahiti2@gmail.com>
2025-02-06 00:08:45 +0530

    Changes

--- src/components/newCollections/Newcollections.jsx
+++ src/components/newCollections/Newcollections.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import "./newcollections.css"
+import new_collection from "../assets/img/new_collections"
+import { Item } from '../Items/Item'
+
+
 
 export const Newcollections = () => {
   return (
-    <div>Newcollections</div>
+    <div className="newCollections">
+      <h1>NEW COLLECTIONS</h1>
+      <hr/>
+      <div className="collections">
+        {new_collection.map((item, x) => {
+          return <Item key={x} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price}  />
+        })}
+      </div>
+    </div>
   )
 }
 
-//1.01.44
+//1.05.33
--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { Banner } from '../../components/MainBanner/Banner'
 import { Popular } from '../../components/Popular/Popular'
 import { Offers } from '../../components/Offers/Offers'
+import { Newcollections } from '../../components/newCollections/Newcollections'
 
 export const Shop = () => {
   return (
     <div>
       <Banner/>
       <Popular/>      
-      <Offers/>x
+      <Offers/>
+      <Newcollections/>
     </div>
   )
 }
